f460395e56b43e29f4160777f8f80e4aecf87420
jjy <1872438001@qq.com>
2013-12-24 14:54:07 +0800

    ——jijunyuan ——12—24

--- iOSReachabilityTestARC/iOSReachabilityTestARC/TMAppDelegate.m
+++ iOSReachabilityTestARC/iOSReachabilityTestARC/TMAppDelegate.m
@@ -17,6 +17,8 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+    
+    //  jijunyuan add some code
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
     self.viewController = [[TMViewController alloc] initWithNibName:@"TMViewController" bundle:nil];
